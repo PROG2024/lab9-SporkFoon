bf3a073d8d0148e497ba4eb89c6a3205c457b2cc
Navin Bunthuphanich <navin.b@ku.th>
2024-03-12 20:28:00 +0700

    Adding 2 unittests to verify that it is a Singleton

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -7,3 +7,34 @@
 
    You can use pytest or unittest.
 """
+import unittest
+from counter import Counter
+
+class TestCounterSingleton(unittest.TestCase):
+
+    def setUp(self):
+        Counter.reset()
+
+    def test_singleton_instance_and_initial_count(self):
+        counter1 = Counter()
+        self.assertEqual(counter1.count, 1)
+        counter1.count
+        self.assertEqual(counter1.count, 1)
+
+    def test_increment_and_shared_state(self):
+      counter1 = Counter()
+      
+      counter1.increment()
+      self.assertEqual(counter1.count, 2)
+
+      counter2 = Counter()
+      self.assertIs(counter1, counter2)
+      self.assertEqual(counter2.count, 2)
+
+      new_count = counter2.increment()
+      self.assertEqual(new_count, 3)
+      self.assertEqual(counter1.count, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
